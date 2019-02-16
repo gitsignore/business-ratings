7fd2e8669d9c7c46e8f6596287d02dab962a9475
gitsignore <maxime.signoret@gmail.com>
2019-02-16 12:01:37 +0100

    Updates

---
+++ precache-manifest.52f8bca258ae8033f42a43156727280f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "070fa69b0f1a9f0c7a93",
+    "url": "/business-ratings/static/js/runtime~main.070fa69b.js"
+  },
+  {
+    "revision": "30846d3a7260ee77a4ee",
+    "url": "/business-ratings/static/js/main.30846d3a.chunk.js"
+  },
+  {
+    "revision": "10b010708dfed8be12ef",
+    "url": "/business-ratings/static/js/2.10b01070.chunk.js"
+  },
+  {
+    "revision": "30846d3a7260ee77a4ee",
+    "url": "/business-ratings/static/css/main.eae1bc62.chunk.css"
+  },
+  {
+    "revision": "f81284e1b07d0f66dd813843af3f995f",
+    "url": "/business-ratings/index.html"
+  }
+];
